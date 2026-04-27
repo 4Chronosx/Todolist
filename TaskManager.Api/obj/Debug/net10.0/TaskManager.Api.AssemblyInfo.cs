16959f09bd195a8ef238fcac837b1c5c8cee652c
Christian James Bayadog <christianjamezz5@gmail.com>
2026-04-27 20:02:05 +0800

    fix: removed reference to client

--- TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7288950ee0a53a8b9a5f9b37e960a1a0cbd02259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4402e3dc661ba451f621079c53744f110c3e8d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
