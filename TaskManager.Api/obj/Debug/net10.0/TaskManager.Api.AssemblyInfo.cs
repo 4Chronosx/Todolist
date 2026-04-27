bea09cb4a7bd33bb0b1939b17497455cf280a219
Christian James Bayadog <christianjamezz5@gmail.com>
2026-04-28 01:40:42 +0800

    chore: adjusted retry time

--- TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71caafc64fade2b99272014ddf508162081ea97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8ed015d1c1905b2230175d68ca2aeba41c066b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
